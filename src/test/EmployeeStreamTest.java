c40617dfc23fb433c1ee1e25215d3c190b0d01f9
SSM <blussm@kakao.com>
2024-04-02 10:56:43 +0900

    EmployeeStreamTest +

--- src/test/EmployeeStreamTest.java
+++ src/test/EmployeeStreamTest.java
@@ -1,10 +1,7 @@
 package test;
 
 import main.Employee;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,8 +40,8 @@ public class EmployeeStreamTest {
      * - stream 요소들의 통계(최대, 최소, 평균값 등)
      * - stream 요소들의 그룹화와 분할
      *
-     * Collectors.toList()
-     * -> 모든 Stream의 요소를 List 인스턴스로 수집하는데 사용할 수 있음.
+     * Collectors.toList() => 모든 Stream의 요소를 List 인스턴스로 수집하는데 사용할 수 있음.
+     *
      * Collectors.groupingBy()
      * -> Collectors의 groupingBy() 또는 groupingByConcurrent() 가 리턴하는 Collector을 매개값으로 대입하여 사용 가능
      *    * groupingBy() : Map 객체 리턴, groupingByConcurrent() : ConcurrentMap 객체 리턴
@@ -55,20 +52,20 @@ public class EmployeeStreamTest {
      *    ex) groupingBy(Employee::getCity, Collectors.mapping(Employee::getName, Collectors.toList())
      */
     @Test
+    @DisplayName("도시 별 사원")
     void group_employees_by_city() {
-        System.out.println("group_employees_by_city");
-
         Map<String, List<Employee>> empByCity;
+
         empByCity = empList.stream()
                 .collect(Collectors.groupingBy(Employee::getCity));
         System.out.println(empByCity);
     }
 
     @Test
+    @DisplayName("특정 도시 출신의 사원")
     void group_employees_by_city_and_get_employee_name() {
-        System.out.println("group_employees_by_city_and_get_employee_name");
-
         Map<String, List<String>> empName;
+
         empName = empList.stream()
                 .collect(Collectors.groupingBy(
                         Employee::getCity,
@@ -78,13 +75,26 @@ public class EmployeeStreamTest {
     }
 
     @Test
+    @DisplayName("나이별 사원")
     void group_employees_by_age() {
-        System.out.println("group_employees_by_age");
-
         Map<Integer, List<Employee>> empByAge;
+
         empByAge = empList.stream()
                 .collect(Collectors.groupingBy(Employee::getAge));
 
         System.out.println(empByAge);
+    }
+
+    @Test
+    @DisplayName("특정 성별을 가진 사원 수")
+    void group_employees_by_sex_and_count_employees() {
+        Map<String, Long> empCountBySex;
+
+        empCountBySex = empList.stream()
+                .collect(Collectors.groupingBy(
+                        Employee::getGender,
+                        Collectors.counting()));
+
+        System.out.println(empCountBySex);
     }
 }
